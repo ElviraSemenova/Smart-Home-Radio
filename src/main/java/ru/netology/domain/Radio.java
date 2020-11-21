cec3847165fe464220e76aee825b29baf357b7c0
Elvira Semenova <shalopayka@gmail.com>
2020-11-21 14:58:40 +0300

    initial commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,67 @@
+package ru.netology.domain;
+
+public class Radio {
+
+    private int currentStation;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int currentVolume;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation == maxStation) {
+            currentStation = minStation;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void previousStation() {
+        if (currentStation == minStation) {
+            currentStation = maxStation;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            currentVolume = maxVolume;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        currentVolume++;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        currentVolume--;
+    }
+
+}
